a0dba1122c9cb6f3865d0c5c610449e322399cc1
lawxls <stanislavchmlv@gmail.com>
2021-04-26 22:56:13 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'mono': ['JetBrains Mono', 'Noto Sans', 'ui-monospace']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
